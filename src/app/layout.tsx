96b46b33cb9489da3fc37dc1f65945e29a180a5c
ivanfa434 <ivanfahrudinaziz123@gmail.com>
2025-03-04 11:23:42 +0700

    feat: add login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ReactQueryProviders from "@/providers/ReactQueryProvider";
+import StoreProvider from "@/providers/StoreProvider";
+import AuthProvider from "@/providers/AuthProvider";
+import { Toaster } from "@/components/ui/sonner"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +31,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ReactQueryProviders>{children}</ReactQueryProviders>
+        <StoreProvider>
+          <AuthProvider>
+            <ReactQueryProviders>{children}</ReactQueryProviders>
+          </AuthProvider>
+        </StoreProvider>
+        <Toaster richColors/>
       </body>
     </html>
   );
